4c76057a22fe01180aef8d39ee0110be2f8a97d7
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2024-05-21 19:47:55 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int param = sc.nextInt();
+
+        Calculable expression = new OOOExpression();								   
+
+        System.out.printf("my expression's result : %d \n", expression.calculate(param));
+    }
+}
+
+interface Calculable {
+    int calculate(int param);
+}
+
+//본인이 작성한 코드를 붙여넣어주세요!
+/*class OOOExpression implements Calculable{
+    @Override
+    public int calculate(int param) {
+        return <코드>;
+    }
+}*/
